e68d23637e1fd5db0268ea1908797ba293cfcd1a
vancebs <vancebs@qq.com>
2019-01-03 09:56:08 +0800

    update docker_entrypoint.sh

--- aosp/docker_entrypoint.sh
+++ aosp/docker_entrypoint.sh
@@ -21,10 +21,10 @@ if [ -z ${GROUP_ID+x} ]; then GROUP_ID=1000; fi
 export CCACHE_DIR=/tmp/ccache
 export USE_CCACHE=1
 
-msg="docker_entrypoint: Creating user UID/GID [$USER_ID/$GROUP_ID]" && echo $msg
+msg="docker_entrypoint: Creating user UID/GID [$USER_ID/$GROUP_ID]" && echo -e "$msg\c"
 groupadd -g $GROUP_ID -r aosp && \
 useradd -u $USER_ID --create-home -r -g aosp aosp
-echo "\r$msg - done"
+echo -e "\r$msg - done"
 
 #msg="docker_entrypoint: Copying .gitconfig and .ssh/config to new user home" && echo $msg
 #cp /root/.gitconfig /home/aosp/.gitconfig && \
@@ -34,18 +34,18 @@ echo "\r$msg - done"
 #chown aosp:aosp -R /home/aosp/.ssh &&
 #echo "$msg - done"
 
-msg="copy configs" && echo $msg
+msg="copy configs" && echo -e "$msg\c"
 cp -rf /data/home/.ssh /home/aosp/
 cp -rf /data/home/.repoconfig /home/aosp/
 cp -rf /data/home/.gitconfig /home/aosp/
 chown -R aosp:aosp /home/aosp/.ssh /home/aosp/.repoconfig /home/aosp/.gitconfig
 chmod 600 /home/aosp/.ssh/id_rsa
-echo "\r$msg - done"
+echo -e "\r$msg - done"
 
-msg="docker_entrypoint: Creating /tmp/ccache and /aosp directory" && echo $msg
+msg="docker_entrypoint: Creating /tmp/ccache and /aosp directory" && echo -e "$msg\c"
 mkdir -p /tmp/ccache /aosp
 chown aosp:aosp /tmp/ccache /aosp
-echo "\r$msg - done"
+echo -e "\r$msg - done"
 
 echo ""
 
